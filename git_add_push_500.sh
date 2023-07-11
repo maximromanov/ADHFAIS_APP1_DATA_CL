cd564164122f36c9381e435df214d145b37d95c9
Maxim Romanov <romanov.maxim@gmail.com>
2023-07-11 12:57:01 +0200

    Adding top 5000 files

--- git_add_push_500.sh
+++ git_add_push_500.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 
-find . -type f | head -n 1000 | xargs git add && git commit -m "Adding top 1000 files" && git push
+find . -type f | head -n 5000 | xargs git add && git commit -m "Adding top 5000 files" && git push
